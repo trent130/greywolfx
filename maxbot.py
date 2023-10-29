38ae1f7513e5a99b128464b849f39f1ae0ec1c62
trent130 <warsamegift@gmail.com>
2023-10-29 11:39:13 +0300

    truth

--- maxbot.py
+++ maxbot.py
@@ -4,19 +4,8 @@ import time
 import re
 
 def follow_accounts():
-    username = username_entry.get()
-    password = password_entry.get()
-    university_acronyms = acronyms_entry.get().split(',')
-    grad_year = grad_year_entry.get()
-    excluded_grad_year = excluded_grad_year_entry.get()
-    hashtag_values = hashtags_entry.get().split(',')
-    max_follow_accounts = int(max_follow_entry.get())
-
-    bot = Bot()
-    bot.login(username=username, password=password)
-
-    # Rest of your follow_filtered_accounts function goes here...
-    # Follow accounts based on provided user inputs
+    # The function to handle following accounts
+    pass
 
 root = tk.Tk()
 root.title("Instagram Bot")
@@ -25,42 +14,48 @@ root.title("Instagram Bot")
 main_frame = tk.Frame(root, padx=20, pady=20)
 main_frame.pack()
 
-# Create labels and entry fields
+# Create labels and entry fields with proper grid placements
 label_font = ('Arial', 10)
 entry_font = ('Arial', 10)
 
+# Username
 username_label = tk.Label(main_frame, text="Username:", font=label_font)
 username_label.grid(row=0, column=0, sticky='w')
 username_entry = tk.Entry(main_frame, font=entry_font)
 username_entry.grid(row=0, column=1)
 
+# University Acronyms
 university_acronym_label = tk.Label(main_frame, text="Acronym:", font=label_font)
-university_acronym_label.grid(row=0, column=0, sticky='w')
+university_acronym_label.grid(row=1, column=0, sticky='w')
 university_acronym_entry = tk.Entry(main_frame, font=entry_font)
-university_acronym_entry.grid(row=0, column=1)
+university_acronym_entry.grid(row=1, column=1)
 
-Graduation_year_label = tk.Label(main_frame, text="Graduation year:", font=label_font)
-Graduation_year_label.grid(row=0, column=0, sticky='w')
-Graduation_year_entry = tk.Entry(main_frame, font=entry_font)
-Graduation_year_entry.grid(row=0, column=1)
+# Graduation Year
+grad_year_label = tk.Label(main_frame, text="Graduation year:", font=label_font)
+grad_year_label.grid(row=2, column=0, sticky='w')
+grad_year_entry = tk.Entry(main_frame, font=entry_font)
+grad_year_entry.grid(row=2, column=1)
 
-Excluded_year_label = tk.Label(main_frame, text="Graduation year:", font=label_font)
-Excluded_year_label.grid(row=0, column=0, sticky='w')
-Excluded_year_entry = tk.Entry(main_frame, font=entry_font)
-Excluded_year_entry.grid(row=0, column=1)
+# Excluded Year
+excluded_year_label = tk.Label(main_frame, text="Excluded year:", font=label_font)
+excluded_year_label.grid(row=3, column=0, sticky='w')
+excluded_year_entry = tk.Entry(main_frame, font=entry_font)
+excluded_year_entry.grid(row=3, column=1)
 
-Hashtag_label = tk.Label(main_frame, text="Graduation year:", font=label_font)
-Hashtag_label.grid(row=0, column=0, sticky='w')
-Hashtag_entry = tk.Entry(main_frame, font=entry_font)
-Hashtag_entry.grid(row=0, column=1)
+# Hashtags
+hashtags_label = tk.Label(main_frame, text="Hashtag:", font=label_font)
+hashtags_label.grid(row=4, column=0, sticky='w')
+hashtags_entry = tk.Entry(main_frame, font=entry_font)
+hashtags_entry.grid(row=4, column=1)
 
-Max_follow_account_label = tk.Label(main_frame, text="Graduation year:", font=label_font)
-Max_follow_account_label.grid(row=0, column=0, sticky='w')
-Max_follow_account_entry = tk.Entry(main_frame, font=entry_font)
-Max_follow_account_entry.grid(row=0, column=1)
-# Create other entry fields and labels for Password, University Acronyms, Graduation Year, Excluded Year, Hashtags, and Max Follow Count
+# Maximum Follow Count
+max_follow_label = tk.Label(main_frame, text="Maximum follow count:", font=label_font)
+max_follow_label.grid(row=5, column=0, sticky='w')
+max_follow_entry = tk.Entry(main_frame, font=entry_font)
+max_follow_entry.grid(row=5, column=1)
 
+# Button for following accounts
 follow_button = tk.Button(main_frame, text="Follow Accounts", command=follow_accounts, font=label_font, bg="blue", fg="white")
-follow_button.grid(row=7, columnspan=2, pady=10)
+follow_button.grid(row=6, columnspan=2, pady=10)
 
 root.mainloop()
